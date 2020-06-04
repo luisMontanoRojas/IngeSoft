1e7f45bc8871adc356d52a1e3c9677f0a91f2f0a
laura Meneses <lmeneses58@gmail.com>
2020-06-03 23:25:26 -0400

    random number teest

--- my-project/spec/numero_random_spec.rb
+++ my-project/spec/numero_random_spec.rb
@@ -1,8 +1,8 @@
 require 'numeroRandom.rb'
 
-#RSpec.describe "Random" do
- #   it 'devuelve numero random' do
-  #      @numeroRandom = NumeroRandom.new
-   #     expect(@numeroRandom.random()).to eq(64)
-   # end
-#end
+RSpec.describe "Random" do
+   it 'devuelve numero random de longitud 4' do
+       @numeroRandom = NumeroRandom.new
+       expect(@numeroRandom.random().size()).to eq(4)
+   end
+end
